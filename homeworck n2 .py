a6e203aeadb7de3474329d7c2660a22bd545f20b
alextsiklauri <tsikalurialex1@gmail.com>
2025-05-08 12:52:20 +0400

    third commit

---
+++ homeworck n2 .py
@@ -0,0 +1,23 @@
+number = 200
+number2 = 20
+print (number + number2)
+
+print (number - number2)
+
+print (number / number2)
+
+print (number * number2)
+
+name = "alex"
+
+print (name * 11)
+
+surname = "wilkauri"
+
+number3 = 100
+
+print (name + surname * number3)
+
+city = "london"
+
+#კონტატიანცია არის ორი ან მეტი სიტყვის ან წინადადების შეერთება მაგალიად: print (name + age)
